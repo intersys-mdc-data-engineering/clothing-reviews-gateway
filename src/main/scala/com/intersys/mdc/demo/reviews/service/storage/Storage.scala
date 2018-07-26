323f0c4099aeca3823d0b6bd3f0a5ecae26e3d3c
rhdzmota <rohdzmota@gmail.com>
2018-07-26 18:08:27 -0500

    create custom graph for mongo and kafka sinks

--- src/main/scala/com/intersys/mdc/demo/reviews/service/storage/Storage.scala
+++ src/main/scala/com/intersys/mdc/demo/reviews/service/storage/Storage.scala
@@ -1,6 +1,6 @@
 package com.intersys.mdc.demo.reviews.service.storage
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorLogging}
 import com.intersys.mdc.demo.reviews.config.{Context, Settings}
 import com.intersys.mdc.demo.reviews.model.{MongoModel, MongoReview}
 import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase}
@@ -9,23 +9,39 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success}
 
+object Storage {
+  case object Ack
+  case object StreamInitialized
+  case object StreamCompleted
+  final case class StreamFailure(ex: Throwable)
+}
 
-class Storage extends Actor with Context {
+class Storage extends Actor with Context with ActorLogging {
+  import Storage._
 
   val mongoClient: MongoClient      = MongoClient(Settings.Mongo.connection)
   val mongoDatabase: MongoDatabase  = mongoClient.getDatabase(Settings.Mongo.db).withCodecRegistry(MongoModel.codecRegistry)
   val mongoCollection: MongoCollection[MongoReview] = mongoDatabase.getCollection(Settings.Mongo.collectionName)
 
   override def receive: Receive = {
+    case StreamInitialized =>
+      log.info("Stream initialized.")
+      sender() ! Ack
+    case StreamCompleted =>
+      log.info("Stream completed")
+      sender() ! Ack
+    case StreamFailure(ex) =>
+      log.info("Stream failure: " + ex.toString)
     case review: MongoReview =>
+      log.info("Receive review:" + review.toString)
       Await.ready(mongoCollection.insertOne(review).toFuture(), Duration.Inf).onComplete {
         case Success(_) =>
-          println("Success:" + review.toString)
+          log.info("Success:" + review.toString)
         case Failure(exception) =>
-          println("Failure: " + exception.toString)
+          log.info("Failure: " + exception.toString)
       }
-      sender() ! review
-
+      sender() ! Ack
     case _ =>
+      log.info("Received unknown object.")
   }
 }
--- src/main/scala/com/intersys/mdc/demo/reviews/service/streaming/Streaming.scala
+++ src/main/scala/com/intersys/mdc/demo/reviews/service/streaming/Streaming.scala
@@ -1,16 +1,16 @@
 package com.intersys.mdc.demo.reviews.service.streaming
 
-import akka.{Done, NotUsed}
-import akka.pattern.ask
+import akka.NotUsed
 import akka.actor.{ActorRef, Props}
-import akka.stream.scaladsl.{Flow, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.GraphDSL.Builder
+import akka.stream.ClosedShape
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source}
 import akka.util.Timeout
 import com.intersys.mdc.demo.reviews.service.pubsub.impl.Kafka
 import com.intersys.mdc.demo.reviews.config._
 import com.intersys.mdc.demo.reviews.model.MongoReview
 import com.intersys.mdc.demo.reviews.service.storage.Storage
 
-import scala.concurrent.Future
 
 case object Streaming extends Context {
   implicit val askTimeout: Timeout = Timeout(Settings.Akka.askTimeout)
@@ -20,17 +20,29 @@ case object Streaming extends Context {
     Source.actorRef[MongoReview](Settings.Akka.Source.bufferSize, Settings.Akka.Source.overflowStrategy)
 
   // Use a publish-subscribe broker as a stream sink
-  val pubSubSink: Sink[String, Future[Done]] = Kafka.publishStringSink
+  val pubSubSink: Sink[MongoReview, NotUsed] = Flow[MongoReview].map(_.toString).to(Kafka.publishStringSink)
 
-  // Flow
-  val parallelism = 100
+  // Use an actor as the streaming sink
   val storageActor: ActorRef = actorSystem.actorOf(Props[Storage])
-  val storageFlow: Flow[MongoReview, String, NotUsed] =
-    Flow[MongoReview].mapAsync(parallelism)(review => (storageActor ? review).mapTo[MongoReview])
-      .map(_.toString)
+  val onErrorMessage: Throwable => Storage.StreamFailure = (ex: Throwable) => Storage.StreamFailure(ex)
+  val storageSink: Sink[MongoReview, NotUsed] = Sink.actorRefWithAck[MongoReview](storageActor,
+    onInitMessage     = Storage.StreamInitialized,
+    ackMessage        = Storage.Ack,
+    onCompleteMessage = Storage.StreamCompleted,
+    onFailureMessage  = onErrorMessage
+  )
 
-  val graph: RunnableGraph[ActorRef] = actorSource.via(storageFlow).to(pubSubSink)
+  // Create the runnable graph (sources to sinks)
+  val graph: RunnableGraph[ActorRef] = RunnableGraph.fromGraph {
+    GraphDSL.create(actorSource) { implicit builder: Builder[ActorRef] => actorSource =>
+      import GraphDSL.Implicits._
+      val bReceivedMongoReview = builder.add(Broadcast[MongoReview](outputPorts = 2))
+
+      actorSource ~>  bReceivedMongoReview ~> pubSubSink
+                      bReceivedMongoReview ~> storageSink
+      ClosedShape
+    }
+  }
 
   val ref: ActorRef = graph.run()
-
 }
